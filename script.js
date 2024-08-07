02272439e476c69803089601d61b565c936bab27
pablogonzamad <pa.gonzalezm@duocuc.cl>
2024-08-07 10:50:22 -0400

    Widget Reloj Funcional

---
+++ script.js
@@ -0,0 +1,26 @@
+// OBTENIENDO ELEMENTOS RELOJ
+const body = document.querySelector("body"),
+    hourHand = document.querySelector(".hour"),
+    minuteHand = document.querySelector(".minute"),
+    secondHand = document.querySelector(".second");
+
+const updateTime = () =>{
+    // OBTENER TIEMPO ACTUAL Y CALCULAR GRADOS PARA MANECILLAS
+    let date = new Date(),
+    secToDeg = (date.getSeconds() / 60) * 360;
+    minToDeg = (date.getMinutes() / 60) * 360;
+    hrToDeg = (date.getHours() / 12) * 360;
+    
+    // ROTAR SECUNDERO SEGUN TIEMPO ACTUAL
+    secondHand.style.transform = `rotate(${secToDeg}deg)`
+    minuteHand.style.transform = `rotate(${minToDeg}deg)`
+    hourHand.style.transform = `rotate(${hrToDeg}deg)`
+
+
+
+}    
+
+// OBTENER DATETIME PARA EL RELOJ    
+setInterval(updateTime, 1000);
+
+// LLAMAR FUNCION UDATETIME AL CARGAR
